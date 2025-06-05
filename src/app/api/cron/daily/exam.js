4de78a2c9c30ee932c757ff13687328c672496b6
iamlive <86843363+Swapnayg@users.noreply.github.com>
2025-06-05 15:41:12 +0530

    updated with exam cron

---
+++ src/app/api/cron/daily/exam.js
@@ -0,0 +1,19 @@
+// app/api/cron/daily/route.js
+import prisma from "./prisma";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+
+  if (secret !== process.env.CRON_SECRET) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
+  }
+
+  console.log("✅ Cron job triggered at", new Date());
+
+ 
+  return new Response(JSON.stringify({ message: "Cron executed" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
