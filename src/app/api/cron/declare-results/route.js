7d9f2296be2d41c9d11c36be80fef2051c6a75b0
iamlive <86843363+Swapnayg@users.noreply.github.com>
2025-06-23 13:53:20 +0530

    updating

--- src/app/api/cron/declare-results/route.js
+++ src/app/api/cron/declare-results/route.js
@@ -4,16 +4,17 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
-
+    console.log(1);
     const now = new Date();
     const tomorrowStart = new Date(now);
     tomorrowStart.setDate(now.getDate() + 1);
     tomorrowStart.setHours(0, 0, 0, 0);
-
+    console.log(2);
     const tomorrowEnd = new Date(tomorrowStart);
     tomorrowEnd.setHours(23, 59, 59, 999);
-
+    console.log(3);
     try {
+        console.log(4);
         // Step 1: Get exam IDs with resultDate == tomorrow
         const exams = await prisma.exam.findMany({
         where: {
@@ -26,12 +27,15 @@ export async function GET() {
             id: true,
         },
         });
+        console.log(5);
 
         const examIds = exams.map((e) => e.id);
-
+        console.log(examIds);
+        console.log(6);
         if (examIds.length === 0) {
             return NextResponse.json({ message: "No exams with resultDate tomorrow." });
         }
+        console.log(7);
         // Step 2: Update related results
         const updateResult = await prisma.result.updateMany({
         where: {
@@ -43,11 +47,9 @@ export async function GET() {
             resultDeclared: true,
         },
         });
+        console.log(7);
         console.log({message: "Results declared successfully.",updatedCount: updateResult.count,examIds,});
-        return NextResponse.json({
-        message: "Results declared successfully.",
-        updatedCount: updateResult.count,
-        examIds,
+        return NextResponse.json({message: "Results declared successfully.", updatedCount: updateResult.count, examIds,
         });
   } catch (error) {
     return NextResponse.json({ error: error instanceof Error ? error.message : error }, { status: 500 });
